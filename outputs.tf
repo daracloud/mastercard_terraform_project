10358e3726fe5cb07b69d93661f30b6748eeffe2
daracloud <84697996+daracloud@users.noreply.github.com>
2022-04-28 14:02:13 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,5 @@
-
+aws_ami_id = ami-00db75007d6c5c578
+launch_template_id = lt-0b04300c8104559d2
+route_table_id = rtb-0ab9e7f21851d7b66
+gateway_id = igw-0f9a1300305948446
+vpc_id = vpc-02474717352be14b6
